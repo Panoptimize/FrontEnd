d36df956f6935c9a2893f3b7516aa46a802ee0b6
lunarmelon <A01662659@tec.mx>
2024-04-23 17:22:32 -0600

    modified table and added storybooks for both components

--- src/components/AgentTable/AgentTable.stories.tsx
+++ src/components/AgentTable/AgentTable.stories.tsx
@@ -1,24 +1,47 @@
-import { Meta, StoryFn } from '@storybook/react';
-import AgentTable from './AgentTable';
+import { Meta, StoryFn } from "@storybook/react";
+import AgentTable from "./AgentTable";
+import React from "react";
+import { IAgentTable } from "./types";
 
 const meta = {
-    title: 'Components/AgentTable',
-    component: AgentTable,
-    parameters: {
-        layout: "centered",
-        docs: {
-            story: {
-                inline: false,
-                description: "Agent table component",
-                iframeHeight: 400,
-            }
-        }
+  title: "Components/AgentTable",
+  component: AgentTable,
+  parameters: {
+    layout: "centered",
+    docs: {
+      story: {
+        inline: false,
+        description: "Agent table component",
+        iframeHeight: 400,
+      },
     },
-    tags: ["autodocs"],
-}as Meta;
+  },
+  argTypes: {
+    row: { control: "array" },
+  },
+  tags: ["autodocs"],
+} as Meta;
 
 export default meta;
 
-/*
-TODO: Finish table component storybook
-*/
+const Templates: StoryFn<IAgentTable> = (args) => <AgentTable {...args} />;
+
+/**
+ * Default story of the AgentTableRow
+ */
+
+export const Default = Templates.bind({});
+Default.args = {
+  rows: [
+    {
+      agentImage:
+        "https://saki.ichoria.org/f/y3gml/Mark_Hamill_by_Gage_Skidmore_2.jpg",
+      name: "Mark Hamill",
+      workspace1: "Sales",
+      workspace2: "Delivery",
+      overallScore: 90,
+      lastActivity: "3 days ago",
+      details: "View Details",
+    },
+  ],
+};
