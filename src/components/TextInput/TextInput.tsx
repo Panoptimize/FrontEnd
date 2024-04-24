14c911f6b4a92d65d157da54d3dfae218ca4c0fb
Esther Ramírez <estherramirez@MacBook-Pro-de-Esther.local>
2024-04-24 11:30:01 -0600

    Text input update

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -8,21 +8,22 @@ export const TextInput: React.FC<ITextInput> = ({
 }) => {
     const [inputValue, setInputValue] = useState('');
     const iconSizeClass = size === 'big' ? 'w-8 h-8' : 'w-6 h-6';
-    const inputHeightClass = size === 'big' ? 'h-40' : 'h-11';
+    const inputHeightClass = size === 'big' ? 'h-40' : 'h-12';
+    const whiteSpaceClass = size === 'big' ? 'whitespace-pre-wrap' : 'whitespace-nowrap';
 
     const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       setInputValue(event.target.value);
     };
-
+    
     return (
         <div className="relative max-w-sm">
             <div className={`flex items-start rounded-sm shadow-sm overflow-hidden border ${inputHeightClass}`}>
-                <img src={icon}  className={`absolute left-2 top-2 ${iconSizeClass}`} />
+                {icon && <img src={icon} className={`absolute left-2 top-2 ${iconSizeClass}`} />}
                 <textarea
                     placeholder={placeholder}
                     value={inputValue}
                     onChange={handleInputChange}
-                    className={`resize-none p-4 border flex-1 py-2 pl-12 pr-4 bg-white outline-none text-gray-700 placeholder-gray-400 w-full ${inputHeightClass}`}
+                    className={`resize-none p-4 border flex-1 py-2 ${icon ? 'pl-12' : 'pl-2'} pr-4 bg-white outline-none text-gray-700 placeholder-gray-400 w-full ${inputHeightClass} ${whiteSpaceClass}`}
                 />
             </div>
         </div>
