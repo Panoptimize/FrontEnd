e661a707a60363dfadd94b2ec41502ed1c211e9d
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2024-04-24 12:55:09 -0600

    Update Sidebar.stories.tsx

--- src/components/Sidebar/Sidebar.stories.tsx
+++ src/components/Sidebar/Sidebar.stories.tsx
@@ -1,22 +1,3 @@
-import { Meta, StoryFn } from '@storybook/react';
-import React from 'react';
-import Sidebar from './Sidebar';
-
-const meta: Meta = {
-  title: 'Components/Sidebar',
-  component: Sidebar,
-  // No argTypes needed as there are no props to control
-  parameters: {
-    layout: 'fullscreen'  // Use the full screen layout to better see the sidebar behavior
-  }
-};
-
-export default meta;
-
-const Template: StoryFn<typeof Sidebar> = () => <Sidebar />;
-export const Default = Template.bind({});
-// No args to pass since there are no props
-
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 
