03aaf7a9abd46eccec093f05b7ac199d2fe885e8
antonioMachorro <a01782114@tec.mx>
2024-04-24 16:06:26 -0600

    Route correcta de imágenes botón

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,7 +9,7 @@ const Button: React.FC<IButton> = ({ text, bold, baseColor, image }) => {
         className={`btn-${baseColor}${bold?'-bold':''} ${text ? 'pr-4':''} ${image && text ? 'pl-1':''} ${!image ? 'pl-4 py-2':''}`}
         
       >
-        {image && <img src={require(`../../Images/${image}.png`)} alt={image} 
+        {image && <img src={require(`../../assets/images/${image}.png`)} alt={image} 
         style={baseColor == 'transparent' ? {filter: 'invert(100%)'} : {} }></img>}
         {text}
       </button>
