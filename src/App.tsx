182f600e021ce63e07f01f807f0138861e78b023
Esther Ramírez <estherramirez@MacBook-Pro-de-Esther.local>
2024-04-24 11:30:40 -0600

    Text input update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TextInput } from './components/TextInput';
 
 function App() {
   return (
-    <div>
-      <TextInput placeholder='Hola' size='small'></TextInput>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
