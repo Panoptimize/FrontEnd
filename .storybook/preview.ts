b61c1f04d60e29e8351a3506d780c8286589df64
CesarCeballosC <105232610+CesarCeballosC@users.noreply.github.com>
2024-04-24 07:59:02 -0600

    Update preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from "@storybook/react";
 import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+import '!style-loader!css-loader!postcss-loader!../src/components/TableAC/TableAC.css';
+import '!style-loader!css-loader!postcss-loader!../src/components/RowAC/RowAC.css';
 
 const preview: Preview = {
   parameters: {
