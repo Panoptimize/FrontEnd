7105bac981bed812fdc3bc191df33f448024c3d9
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-04-29 09:31:02 -0600

    choice box con filtrado de input
    necesita la libreria react-select

--- src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
+++ src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
@@ -1,25 +1,43 @@
 import React from "react";
 import './index.css';
 import { IChoiceBox } from "./types";
+import Select from 'react-select';
+
+
+const customStyles = {
+  control: () => ({
+    
+    display: 'flex',
+    width: 150,
+    fontSize: '0.875rem',
+    fontWeight: 500,
+    lineHeight: '1.25rem',
+    color: '#111827',
+    borderRadius: 0, // Para eliminar el borde redondeado
+  }),
+};
+
 
 const ChoiceBox: React.FC<IChoiceBox> = ({ boxText, options }) => {
-    return (
-      <div className="starterChoice">
-        <label htmlFor="labelChoice">
-          {boxText}
-        </label>
-        <select
-          id="options"
-          className="choiceBox"
-        >
-          {options.map((option, index) => (
-            <option key={index} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
-    );
-  };
+  const selectOptions = options.map(option => ({
+    value: option.value,
+    label: option.label
+  }));
+
+  return (
+    <div className="starterChoice">
+      <label htmlFor="labelChoice">
+        {boxText}
+      </label>
+      <Select
+        id="options"
+        className="choiceBox"
+        options={selectOptions}
+        styles={customStyles}
+      />
+    </div>
+  );
+};
 
 export default ChoiceBox;
+
