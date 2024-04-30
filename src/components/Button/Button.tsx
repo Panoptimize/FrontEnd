962cc157ad124621f8e467f6f0d47106bb7405f7
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2024-04-29 18:34:46 -0600

    update a SideBar, algo de css de buttons, imgs correctos

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import { IButton } from './types';
-import './Button.css';
+import React from "react";
+import { IButton } from "./types";
+import "./Button.css";
 
 const Button: React.FC<IButton> = ({ text, bold, baseColor, image }) => {
   return (
     <>
-      <button 
-        className={`btn-${baseColor}${bold?'-bold':''} ${text ? 'pr-4':''} ${image && text ? 'pl-1':''} ${!image ? 'pl-4 py-2':''}`}
-        
+      <button
+        className={`btn-${baseColor}${bold ? "-bold" : ""} ${text ? "pr-4" : ""} ${image && text ? "pl-1" : ""} ${!image ? "pl-4 py-2" : ""}`}
       >
-        {image && <img src={require(`../../assets/images/${image}.png`)} alt={image} 
-        style={baseColor == 'transparent' ? {filter: 'invert(100%)'} : {} }></img>}
+        {image && (
+          <img
+            src={require(`../../assets/images/${image}.png`)}
+            alt={image}
+            style={baseColor == "transparent" ? { filter: "invert(100%)" } : {}}
+          ></img>
+        )}
         {text}
       </button>
     </>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,37 +1,41 @@
 import React from "react";
-
+import { Button } from "../Button";
+//transparent history anda al revés. sale blanca transparente
+//button gray hover más clarito
+//hacer vairante menta claro para botones solo del sidebar
+//agregar imagen de download, settings, logout
 const Sidebar = () => {
   return (
-    <div className="w-14 h-screen justify-center bg-teal-100 flex transition ease-in-out delay-150 hover:w-40 duration-300">
+    <div className="w-14 h-screen justify-center bg-teal-100 flex transition ease-in-out delay-150 duration-300">
       <div className="flex flex-col justify-between">
         <div className="flex flex-col my-1 justify-start">
-          <div className="mb-4 h-20 w-14 hover:w-36 bg-blue-700 hover:bg-blue-950">
-            PZ
+          <div className="mb-4 py-2 flex flex-auto justify-center bg-blue-200 hover:bg-blue-500">
+            <Button baseColor="rose"></Button>
           </div>
-          <div className="h-14 w-14 hover:w-36 bg-amber-700 hover:bg-amber-950">
-            OV
+          <div className=" flex flex-auto justify-center ">
+            <Button baseColor="transparent" image="dashboard"></Button>
           </div>
           <div className="flex flex-col my-9">
-            <div className="my-1 h-14 w-14 hover:w-36 bg-pink-600 hover:bg-pink-900">
-              DB
+            <div className="my-1 flex flex-auto">
+              <Button baseColor="teal" image="dashboard"></Button>
             </div>
-            <div className="my-1 h-14 w-14 hover:w-36 bg-pink-600 hover:bg-pink-900">
-              AC
+            <div className="my-1 flex flex-auto">
+              <Button baseColor="transparent" image="actionCenter"></Button>
             </div>
-            <div className="my-1 h-16 w-14 hover:w-36 bg-pink-600 hover:bg-pink-900">
-              AG
+            <div className="my-1 flex flex-auto">
+              <Button baseColor="transparent" image="agents"></Button>
             </div>
-            <div className="my-1 h-16 w-14 hover:w-36 bg-pink-600 hover:bg-pink-900">
-              HS
+            <div className="my-1 flex flex-auto">
+              <Button baseColor="transparent" image="history"></Button>
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-end">
-          <div className="my-1 h-16 w-14 hover:w-36 bg-green-600 hover:bg-green-900">
-            AJ
+        <div className="flex flex-col justify-end my-4">
+          <div className="my-1 flex flex-auto">
+            <Button baseColor="rose" image="history"></Button>
           </div>
-          <div className="my-1 h-16 w-14 hover:w-36 bg-green-600 hover:bg-green-900">
-            LG
+          <div className="my-1  flex flex-auto">
+            <Button baseColor="gray" image="dashboard"></Button>
           </div>
         </div>
       </div>
