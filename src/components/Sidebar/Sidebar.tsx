13729de28bacb8aacf9953636f7fcdd440f8009d
Carava Jojo <102180250+InakiVA@users.noreply.github.com>
2024-04-29 20:51:07 -0600

    cambio a formato de sidebar, color de topbar, flex de buttons, sidebar recibe variable para saber si está expandida o no

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,41 +1,65 @@
 import React from "react";
 import { Button } from "../Button";
-//transparent history anda al revés. sale blanca transparente
-//button gray hover más clarito
-//hacer vairante menta claro para botones solo del sidebar
-//agregar imagen de download, settings, logout
-const Sidebar = () => {
+/*
+
+
+*/
+
+interface SidebarProps {
+  expanded?: boolean;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ expanded = true }) => {
+  const width = expanded ? "w-48" : "w-14";
   return (
-    <div className="w-14 h-screen justify-center bg-teal-100 flex transition ease-in-out delay-150 duration-300">
-      <div className="flex flex-col justify-between">
+    <div className={`${width} flex flex-auto px-2 justify-center bg-teal-100`}>
+      <div className="flex flex-col flex-auto justify-between">
         <div className="flex flex-col my-1 justify-start">
-          <div className="mb-4 py-2 flex flex-auto justify-center bg-blue-200 hover:bg-blue-500">
-            <Button baseColor="rose"></Button>
-          </div>
-          <div className=" flex flex-auto justify-center ">
-            <Button baseColor="transparent" image="dashboard"></Button>
-          </div>
-          <div className="flex flex-col my-9">
+          <div className="flex flex-col">
             <div className="my-1 flex flex-auto">
-              <Button baseColor="teal" image="dashboard"></Button>
+              <Button
+                baseColor="teal"
+                image="dashboard"
+                text={expanded ? "Dashboard" : ""}
+              ></Button>
             </div>
             <div className="my-1 flex flex-auto">
-              <Button baseColor="transparent" image="actionCenter"></Button>
+              <Button
+                baseColor="transparent"
+                image="actionCenter"
+                text={expanded ? "Action Center" : ""}
+              ></Button>
             </div>
             <div className="my-1 flex flex-auto">
-              <Button baseColor="transparent" image="agents"></Button>
+              <Button
+                baseColor="transparent"
+                image="agents"
+                text={expanded ? "Agents" : ""}
+              ></Button>
             </div>
             <div className="my-1 flex flex-auto">
-              <Button baseColor="transparent" image="history"></Button>
+              <Button
+                baseColor="transparent"
+                image="history"
+                text={expanded ? "History" : ""}
+              ></Button>
             </div>
           </div>
         </div>
         <div className="flex flex-col justify-end my-4">
           <div className="my-1 flex flex-auto">
-            <Button baseColor="rose" image="history"></Button>
+            <Button
+              baseColor="gray"
+              image="history"
+              text={expanded ? "Settings" : ""}
+            ></Button>
           </div>
           <div className="my-1  flex flex-auto">
-            <Button baseColor="gray" image="dashboard"></Button>
+            <Button
+              baseColor="rose"
+              image="dashboard"
+              text={expanded ? "Log Out" : ""}
+            ></Button>
           </div>
         </div>
       </div>
