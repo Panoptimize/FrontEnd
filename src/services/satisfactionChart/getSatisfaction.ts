4e33388d44eb375b0f212193f32c03224ebe444d
antonioMachorro <a01782114@tec.mx>
2024-05-02 12:31:49 -0600

    Update getSatisfaction.ts

--- src/services/satisfactionChart/getSatisfaction.ts
+++ src/services/satisfactionChart/getSatisfaction.ts
@@ -4,11 +4,9 @@ export const getSatisfaction = async () => {
     let res: any
     const endpoint = 'customer-satisfaction';
     await httpInstance.get(endpoint).then((data) => {
-        console.log(data)
         res = data;
     }).catch((err) => {
         res = err.response
     });
-    console.log(res)
     return res;
 }
