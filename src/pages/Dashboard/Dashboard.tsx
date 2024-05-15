2b75983b931fc4b007efc09ac731af95a8f407a1
EstherRC <a01656060@tec.mx>
2024-05-14 19:39:03 -0600

    Status from connect

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -20,38 +20,13 @@ import { IStatusCard } from '../../components/StatusCard/types';
 
 export const Dashboard: React.FC = () => {
 
-    const [satisfactionLevels, setSatisfactionLevels] = useState<number[]>([]);
-    const [contactMediumData, setContactMediumData] = useState<number[]>([]);
-    const [activityData, setActivityData] = useState<number[]>([]);
+    
 
-    const users = [
-        { username: "Mariah Carey",     data: [0, 10, 5, 2, 20, 30, 45] },
-        { username: "Will Smith",       data: [0, 5, 10, 15, 20, 25, 30] },
-        { username: "Tom Cruise",       data: [0, 10, 15, 20, 25, 30, 35] },
-        { username: "Mariah Carey",     data: [0, 10, 5, 2, 20, 30, 45] },
-        { username: "Will Smith",       data: [0, 5, 10, 15, 20, 25, 30] },
-        { username: "Tom Cruise",       data: [0, 10, 15, 20, 25, 30, 35] },
-        { username: "Mariah Carey",     data: [0, 10, 5, 2, 20, 30, 45] },
-        { username: "Will Smith",       data: [0, 5, 10, 15, 20, 25, 30] },
-        { username: "Tom Cruise",       data: [0, 10, 15, 20, 25, 30, 35] },
-    ];
-    const [kpiData, setKpiData] = useState<KpiData>();
-
-    const fetchContactMedium = async () => {
-        try {
-            const response = await getContactMedium();
-            if (response && response.data) {
-                setContactMediumData(response.data);
-            }
-        } catch (error) {
-            console.error("Error al obtener datos de medios de contacto:", error);
-        }
-    };
-
+    
     const [status, setStatus] = useState<IStatusCard[]>([]);
 
     const getAgentsStatus = async () => {
-        const result = await getStatus();
+        const result = await getStatus("7c78bd60-4a9f-40e5-b461-b7a0dfaad848");
         if (result.error) {
             console.error(result.error);
         } else {
@@ -59,49 +34,17 @@ export const Dashboard: React.FC = () => {
         }
     };
     
-      useEffect(() => {
-   
-        getAgentsStatus();
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            getAgentsStatus();
+        }, 5000);
+    
+        return () => clearInterval(intervalId);
     }, []);
 
-    const getSatisfactionLevels = async () => {
-        try {
-            const data = await getSatisfaction();
-            if (data) {
-                setSatisfactionLevels(data);
-            }
-        } catch (error) {
-            console.error("Error al obtener los niveles de satisfacción:", error);
-        }
-    };
-    const getKpiData = async () => {
-        const result = await getKpis();
-        if (result.error) {
-            console.error(result.error);
-        } else {
-            // Solo actualiza el estado si result.data no es null
-            if (result.data) {
-                setKpiData(result.data);
-            }
-        }
-    };
-
-
-    const fetchActivityData = async () => {
-        try {
-            const data = await getMonthlyActivity();
-            setActivityData(data);
-        } catch (error) {
-            console.error("Error al obtener datos de actividad mensual:", error);
-        }
-    };
-
+   
     useEffect(() => {
-        fetchContactMedium();
-        getKpiData();
         getAgentsStatus();
-        getSatisfactionLevels();
-        fetchActivityData();
     }, []);
 
     return (
@@ -124,36 +67,7 @@ export const Dashboard: React.FC = () => {
                     ))}
            
                 </div>
-
-                <div className="font-poppins px-6">
-                    <p className="text-gray-600 pt-2 px-4 text-lg"> Overall Performance </p>
-                </div>
-                {/* Charts */}
-                <div className="flex flex-row justify-between items-stretch w-full pt-4 px-16">
-                        <SatisfactionChart data={satisfactionLevels}/>
-                        <ContactMedium data = {contactMediumData}/>
-                        <div>
-                            {kpiData && (
-                                <div>
-                                    <div className="flex flex-row space-x-6">
-                                        <DataCard title="Avg Hold Time" content={`${kpiData?.avgHoldTime} seconds`} />
-                                        <DataCard title="First Contact Resolution" content={`${kpiData?.firstcontactresolution}%`} />
-                                        <DataCard title="Abandonment Rate" content={`${kpiData?.abandonmentRate}%`} />
-                                    </div>
-                                    <div className="flex flex-row space-x-6 pt-5">
-                                        <DataCard title="Service Level" content={`${kpiData?.serviceLevel}%`} />
-                                        <DataCard title="Agent Schedule Adherence" content={`${kpiData?.agentScheduleAdherence}%`} />
-                                        <DataCard title="Avg Speed Answer" content={`${kpiData?.avgSpeedAnswer} seconds`} />
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                </div>
-                {/* Second row of charts */}
-                <div className="flex flex-row justify-between items-stretch space-x-6 pt-6 px-16">
-                    <PerformanceChart users={users} />
-                    <ActivityChart data={activityData}/>
-                </div>
+                
             </div>
         </div>
     );
