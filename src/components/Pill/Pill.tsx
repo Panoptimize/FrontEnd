85c16b447b5dba4d98cf6e1e79856f456ff5b3e5
BillySh <A01657143@tec.mx>
2024-05-19 14:12:43 -0600

    pillVariant

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import { IPill } from './types';
 import './Pill.css'
 
-const Pill: React.FC<IPill> = ({
-    title,
-})=>{
+const Pill: React.FC<IPill> = ({title, icon,})=>{
+    const isIcon = icon?.endsWith('.svg');
+    if (icon && !isIcon) {
+        console.error('simple pill');
+        return null;
+    }
+
     return(
         <div className='flex'>
-            <div className='pill'>
+            <div className={`pill ${!icon ? 'pl-4 py-2' : ''}`}>
                 {title}
+                {icon &&(
+                    <img
+                    // Work only with SVG files
+                    src={require(`../../assets/images/${icon}`)}
+                    //src={require(`../../assets/images/${image}${isSvg ? '' : '.png'}`)}
+                    alt={icon}
+                    />
+                )}
             </div>
         </div>
     )
