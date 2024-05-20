5dc71b2adb88fb8d3f6507e2a1553c4b98c8851d
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-05-20 16:51:04 -0600

    Flex holder terminado para opciones multiples

--- src/components/FlexHolder/FlexHolder.tsx
+++ src/components/FlexHolder/FlexHolder.tsx
@@ -1,17 +1,16 @@
 import React, { ReactNode } from 'react';
 import { IFlexHolder } from './types';
 
-const FlexHolder: React.FC<IFlexHolder> = ({ components }) => (
-    <div className="w-full h-full overflow-hidden bg-white m-0" style={{ width: '150px', height: '85px', border: '1px solid rgba(128, 128, 128, 0.5)', overflowX: 'scroll' }}>
+const FlexHolder: React.FC<IFlexHolder> = ({ components, border }) => (
+    <div className="flex flex-auto px-3 rounded-3xl overflow-hidden bg-white" style={{overflowX: 'scroll', border: border ? '1px solid rgba(128, 128, 128, 0.25)' : 'none'}}>
         <div className="flex space-x-4">
             {components.map((component, index) => (
-                <React.Fragment key={index}>
-                    {component}
-                </React.Fragment>
+                <div className='mx-2' key={index}>
+                        {component}
+                </div>
             ))}
         </div>
     </div>
 );
 
 export default FlexHolder;
-
--- src/components/FlexHolder/types.ts
+++ src/components/FlexHolder/types.ts
@@ -2,5 +2,6 @@ import React, { ReactNode } from 'react';
 
 
 export interface IFlexHolder {
-    components: ReactNode[];
+    components: ReactNode[]; //Los componentes que tendra dentro del holder
+    border?: Boolean; //True si quieres borde, falso/nada si no quieres borde 
 }
