773aa1d35d4cb929610a6b1c8a35256aefcedc12
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-05-20 15:12:38 -0600

    cambios a flex holder

--- src/components/FlexHolder/FlexHolder.tsx
+++ src/components/FlexHolder/FlexHolder.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import { IFlexHolder } from './types';
 
 const FlexHolder: React.FC<IFlexHolder> = ({ components }) => (
-    <div className="w-full h-full overflow-hidden bg-white m-0">
+    <div className="w-full h-full overflow-hidden bg-white m-0" style={{ width: '150px', height: '85px', border: '1px solid rgba(128, 128, 128, 0.5)', overflowX: 'scroll' }}>
         <div className="flex space-x-4">
             {components.map((component, index) => (
                 <React.Fragment key={index}>
@@ -14,3 +14,4 @@ const FlexHolder: React.FC<IFlexHolder> = ({ components }) => (
 );
 
 export default FlexHolder;
+
