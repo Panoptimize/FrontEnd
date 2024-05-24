a3f992a032b0c381b85c9b6a084367d98adfa1b6
lunarmelon <A01662659@tec.mx>
2024-05-23 20:34:47 -0600

    fix conflicts v5

--- src/pages/Dashboard/statustype.ts
+++ src/pages/Dashboard/statustype.ts
@@ -2,13 +2,9 @@ export interface IStatus {
   /**
    * Activity that the agents are performing, is restricted to only the activites allowed
    */
-  status:
-    | "AGENTS_AVAILABLE"
-    | "AGENTS_ON_CONTACT"
-    | "AGENTS_AFTER_CONTACT_WORK"
-    | "AGENTS_ONLINE";
+  status: 'AGENTS_AVAILABLE' | 'AGENTS_ON_CONTACT' | 'AGENTS_AFTER_CONTACT_WORK' | 'AGENTS_ONLINE';
   /**
-   * Current amount of agents in this activity
+   * Current amount of agents in this activity 
    */
-  numUsers: number;
+  numUsers : number;
 }
---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+export {getStatus} from './status/getStatus';
+export {getContactMedium} from './contactMedium/getContactMedium'
+export {getSatisfaction} from './satisfactionChart/getSatisfaction'
+export {getMonthlyActivity} from './activityChart/getMonthlyActivity'
