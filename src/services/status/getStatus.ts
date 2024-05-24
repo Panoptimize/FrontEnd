e4a39fab84806a0579546e89816c680b358963d1
lunarmelon <A01662659@tec.mx>
2024-05-23 20:20:00 -0600

    fix conflicts v3

--- src/services/status/getStatus.ts
+++ src/services/status/getStatus.ts
@@ -3,31 +3,37 @@ import { IStatusCard } from "../../components/StatusCard/types";
 import { IStatus } from "../../pages/Dashboard/statustype";
 
 export const getStatus = async (instanceId: string) => {
-  try {
-    const response = await httpInstance.get(`status?instanceId=${instanceId}`);
-    console.log("Response data:", response.data);
-    const processedData = processMetrics(response.data);
-    console.log("Processed data:", processedData);
-    return { data: processedData, error: null };
-  } catch (err) {
-    console.error("Error fetching status:", err);
-    return {
-      data: [],
-      error: (err as any).response || { message: "An unknown error occurred" },
-    };
-  }
-};
+    try {
+        const response = await httpInstance.get(`status?instanceId=${instanceId}`);
+        console.log('Response data:', response.data);  
+        const processedData = processMetrics(response.data);
+        console.log('Processed data:', processedData); 
+        return { data: processedData, error: null };
+    } catch (err) {
+        console.error('Error fetching status:', err); 
+        return { data: [], error: (err as any).response || { message: 'An unknown error occurred' } };
+    }
+}
 
 const processMetrics = (data: MetricData): IStatusCard[] => {
-  if (data && Array.isArray(data)) {
-    return data.map((item) => ({
-      status: item.metricName,
-      numUsers: item.metricValue,
-    }));
-  } else {
-    return [];
-  }
-};
+    if (data && Array.isArray(data)) {
+        return data.map(item => ({
+            status: item.metricName,  
+            numUsers: item.metricValue
+        }));
+    } else {
+        return [];  
+    }
+}
+
+export default getStatus;
+
+
+interface MetricData {
+    MetricResults: Array<{
+        Collections: IStatus[];
+    }>;
+}
 
 export default getStatus;
 
