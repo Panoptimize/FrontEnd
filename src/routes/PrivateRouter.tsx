1668f0cd384a369944ff1a0651707e8f57c058a7
lunarmelon <A01662659@tec.mx>
2024-05-23 20:23:39 -0600

    fix conflicts v4

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { Sidebar } from "../components/Sidebar";
+import { Topbar } from "../components/Topbar";
 
-const PrivateRouter: React.FC = () => {
+const PrivateRouter = () => {
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarExpanded(!isSidebarExpanded);
+  };
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="flex flex-col flex-initial h-screen">
+      <Topbar toggleSidebar={toggleSidebar} />
+      <div className="h-dvh flex flex-row flex-auto">
+        <div>
+          <Sidebar expanded={isSidebarExpanded} />
+        </div>
+        <div className="flex flex-col flex-auto h-[calc(100vh-65px)]        ">
+          <div className="flex flex-auto bg-white overflow-y-scroll">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
