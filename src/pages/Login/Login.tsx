0c73ba1931de3ec9ffb82200981e527811bef399
lunarmelon <A01662659@tec.mx>
2024-05-23 18:06:13 -0600

    update

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,7 @@
 import Background from "../../assets/images/AuthBackground.png";
-import Logo from "../../assets/images/PanoptimizeVertical.png";
+import Email from "../../assets/images/email.png";
+import Password from "../../assets/images/password.png";
+import Logo from "../../assets/images/PanoptimizeBlue.png";
 import { Button } from "../../components/Button";
 import React, { useState } from "react";
 import { useAppContext } from "../../store/app-context/app-context";
@@ -32,13 +34,9 @@ const Login = () => {
   return (
     <div>
       <div className="relative w-full h-screen overflow-hidden">
-        <img
-          src={Background}
-          alt="Background"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+        <img src={Background} alt="Background" />
         <div className="absolute inset-0 flex items-center justify-center space-x-0">
-          <div className="w-96 h-96 bg-blue-300 rounded-l-xl shadow-lg flex items-center justify-center">
+          <div className="w-96 h-96 bg-cyan-100 rounded-l-xl shadow-lg flex items-center justify-center">
             <img src={Logo} alt="Panoptimize logo" className="w-80" />
           </div>
           <div className="w-96 h-96 bg-white rounded-r-xl shadow-lg flex flex-col items-center justify-center">
@@ -46,7 +44,11 @@ const Login = () => {
               {error && (
                 <p className="text-red-500 mb-4 text-sm font-medium">{error}</p>
               )}
-              <form onSubmit={handleSubmit}>
+              <p className="text-xl font-bold">Company name</p>
+              <form
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center"
+              >
                 <div className="mb-4">
                   <label
                     htmlFor="email"
@@ -54,14 +56,19 @@ const Login = () => {
                   >
                     Email:
                   </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="email@test.com"
-                    className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-blue-400"
-                    onChange={handleChange}
-                  />
+                  <div className="relative">
+                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                      <img src={Email} alt="email" className="h-5 w-5" />
+                    </span>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="email@test.com"
+                      className="pl-10 w-full px-3 py-2 border border-gray-300 rounded focus:outline-blue-400"
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
                 <div className="mb-4">
                   <label
@@ -70,21 +77,21 @@ const Login = () => {
                   >
                     Password:
                   </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="******"
-                    className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-blue-400"
-                    onChange={handleChange}
-                  />
+                  <div className="relative">
+                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                      <img src={Password} alt="password" className="h-5 w-5" />
+                    </span>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="******"
+                      className="pl-10 w-full px-3 py-2 border border-gray-300 rounded focus:outline-blue-400"
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-                <Button
-                  baseColor="mint"
-                  text={"Log In"}
-                  type="submit"
-                  bold={true}
-                ></Button>
+                <Button baseColor="teal" text={"Log In"} type="submit"></Button>
               </form>
             </div>
           </div>
