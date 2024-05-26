9118750b3c3934e65542b0a279f3b5abcf97c8a6
lunarmelon <A01662659@tec.mx>
2024-05-26 16:19:37 -0600

    added loading page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,8 @@
-export { Dashboard } from './Dashboard';
-export { ActionCenter } from './ActionCenter';
-export { Agents } from './Agents';
-export { History } from './History';
-export { Settings } from './Settings';
+export { Dashboard } from "./Dashboard";
+export { ActionCenter } from "./ActionCenter";
+export { Agents } from "./Agents";
+export { History } from "./History";
+export { Settings } from "./Settings";
+export { Loading } from "./Loading";
+export { Login } from "./Login";
+
--- src/routes/PublicRouter.tsx
+++ src/routes/PublicRouter.tsx
@@ -1,5 +1,5 @@
-import exp from "constants";
 import { Outlet } from "react-router-dom";
+import Loading from "../pages/Loading/Loading";
 import { useAppContext } from "../store/app-context/app-context";
 
 const PublicRouter = () => {
@@ -8,7 +8,7 @@ const PublicRouter = () => {
   return (
     <div>
       {loadingContext ? (
-        <h1>Loading...</h1>
+        <Loading />
       ) : (
         <>
           <Outlet />
@@ -19,4 +19,3 @@ const PublicRouter = () => {
 };
 
 export default PublicRouter;
-
