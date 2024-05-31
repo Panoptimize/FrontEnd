e85272c255a6aecb85df20073ec8105d01ebfe32
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-05-31 12:48:35 -0600

    cambios para que no use css

--- src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
+++ src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
@@ -3,9 +3,9 @@ import { IChoiceBox } from "./types";
 
 const ChoiceBox: React.FC<IChoiceBox> = ({ boxText, options, chosen }) => {
   return (
-    <div className="flex flex-auto flex-h border border-gray-300 bg-white px-[15px] p-1 rounded-full space-x-2">
-      <label className="text-sm leading-5 font-medium text-gray-900 mr-2 pr-2.5">{boxText}</label>
-      <select id="options" className="flex text-sm font-medium leading-5 text-gray-900 ml-0.5" defaultValue = {chosen}>
+    <div className="flex w-30 h-8 p-1 rounded-full border border-gray-400 bg-white pl-4 pr-4">
+      <label className="w-12 mr-2 text-sm leading-5 font-medium text-gray-900 pr-2">{boxText}</label>
+      <select id="options" className="flex w-25 text-sm font-medium leading-5 text-gray-900" defaultValue = {chosen}>
         {options.map((option, index) =>   (
           <option key={index} value={option.value}>
             {option.label}
