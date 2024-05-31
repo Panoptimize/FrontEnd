79a4141cfbf2fcb60225f5a5880de185457a5e65
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-05-30 20:18:06 -0600

    cambios

--- src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
+++ src/components/ChoiceBoxes/ChoiceBox/ChoiceBox.tsx
@@ -3,10 +3,10 @@ import { IChoiceBox } from "./types";
 
 const ChoiceBox: React.FC<IChoiceBox> = ({ boxText, options, chosen }) => {
   return (
-    <div className="flex flex-auto  border border-gray-300 bg-white px-[15px] p-1 rounded-full space-x-2">
+    <div className="flex flex-auto flex-h border border-gray-300 bg-white px-[15px] p-1 rounded-full space-x-2">
       <label className="text-sm leading-5 font-medium text-gray-900 mr-2 pr-2.5">{boxText}</label>
       <select id="options" className="flex text-sm font-medium leading-5 text-gray-900 ml-0.5" defaultValue = {chosen}>
-        {options.map((option, index) => (
+        {options.map((option, index) =>   (
           <option key={index} value={option.value}>
             {option.label}
           </option>
