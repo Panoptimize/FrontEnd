7d8e6268251b1d8af63f4d271f506518c68b589d
PabloCeb2000 <113377312+PabloCeb2000@users.noreply.github.com>
2024-06-02 16:43:02 -0600

    Post method added to the function 'getDownload' (We just need the data from the dashboard filters and it is over)

--- src/services/Download/getDownload.ts
+++ src/services/Download/getDownload.ts
@@ -1,16 +1,33 @@
 import httpInstance from "../httpInstance";
 
+interface DownloadDTO {
+    instanceId: string;
+    startDate: string;
+    endDate: string;
+    routingProfiles: string[];
+    queues: string[];
+    agents: string[];
+  }
 export const getDownload = async () => {
     const endpoint = '/download/getDownload';
     const date = new Date();
+    const downloadDTO: DownloadDTO = {
+        instanceId: '7c78bd60-4a9f-40e5-b461-b7a0dfaad848',
+        startDate: '2024-05-01',
+        endDate: '2024-05-31',
+        routingProfiles: ['4896ae34-a93e-41bc-8231-bf189e7628b1'],
+        queues: [],
+        agents: []
+    };
 
     try {
-        const response = await httpInstance.get(endpoint, { responseType: 'blob' });
+        const response = await httpInstance.post(endpoint, downloadDTO, { responseType: 'blob' });
+   
         if (response.data) {
-            // Create a blob from the response data
+            
             const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
             const url = window.URL.createObjectURL(blob);
-            // Create an anchor element and set its href to the blob URL
+            
             const link = document.createElement('a');
             link.href = url;
             link.download = 'DataReport_' + date.getTime() + '.xlsx'; 
