5ae3092068ab7c977b59b100998dc1769b730a2f
antonioMachorro <a01782114@tec.mx>
2024-06-03 17:36:35 -0600

    ChoiceBox update to useRef

--- src/components/ChoiceBoxes/ChoiceBox/types.ts
+++ src/components/ChoiceBoxes/ChoiceBox/types.ts
@@ -1,6 +1,11 @@
+import { Priority } from "../../../constants/Priority";
 
 export interface IChoiceBox{
     boxText?:string
     options: {value: string, label: string }[] //no puede ser undefined
     chosen?: any
+}
+
+export interface ChoiceBoxRef{
+    getValue: () => Priority;
 }
--- src/components/NoteInputs/NoteInputs.tsx
+++ src/components/NoteInputs/NoteInputs.tsx
@@ -9,6 +9,7 @@ import { TextInputRef } from "../TextInput/types";
 import { Priority } from "../../constants/Priority";
 import { data } from "autoprefixer";
 import { error } from "console";
+import { ChoiceBoxRef } from "../ChoiceBoxes/ChoiceBox/types";
 
 /* Ahorita se esta simulando lo del backend, ya despues seria con un backend ya implementado */
 
@@ -19,15 +20,22 @@ const NoteInputs: React.FC<INoteInputs> = ({ id, priority, title, text }) => {
 
   const nameRef = useRef<TextInputRef>(null);
   const descriptionRef = useRef<TextInputRef>(null);
+  const priorityRef = useRef<ChoiceBoxRef>(null);
 
   const createEditNote = () => {
+    console.log("ENTRA A ESTE METODO")
+    console.log(nameRef.current?.getValue())
+    console.log(descriptionRef.current?.getValue())
+    console.log(priorityRef.current?.getValue())
+    
     const name = nameRef.current?.getValue ? nameRef.current.getValue() : "";
     const desc = descriptionRef.current?.getValue ? descriptionRef.current.getValue() : "";
+    const priority = priorityRef.current?.getValue ? priorityRef.current.getValue() : Priority.low;
 
     const updatedNote:INote = {
       name: name ? name : "",
       description: desc ? desc : "",
-      priority: Priority.high,
+      priority: priority,
       solved: false,
     }
 
@@ -41,6 +49,13 @@ const NoteInputs: React.FC<INoteInputs> = ({ id, priority, title, text }) => {
       console.error(error)
     });
   };
+
+  const checkInputs =() => {
+    console.log("ENTRA A ESTE METODO")
+    console.log(nameRef.current?.getValue())
+    console.log(descriptionRef.current?.getValue())
+    console.log(priorityRef.current?.getValue())
+  }
 
   return (
     <div className="flex flex-auto flex-col w-full h-full p-2 space-y-4">
@@ -56,22 +71,23 @@ const NoteInputs: React.FC<INoteInputs> = ({ id, priority, title, text }) => {
         <ChoiceBox
           boxText="Priority:"
           options={[
-            { value: "Low", label: "Low" },
-            { value: "Medium", label: "Medium" },
-            { value: "High", label: "High" },
+            { value: "low", label: "Low" },
+            { value: "medium", label: "Medium" },
+            { value: "high", label: "High" },
           ]}
           chosen = {priority}
+          ref = {priorityRef}
         ></ChoiceBox>
       </div>
       <div className="h-full">
-        <TextInput placeholder="Add Text" size="big" text={text}></TextInput>
+        <TextInput placeholder="Add Text" size="big" text={text} ref={descriptionRef}></TextInput>
       </div>
       <div className="grid grid-cols-3">
         <div></div>
         <div></div>
         <div className="flex flex-row space-x-4">
           <Button baseColor="rose" image="Cross.svg" text="Delete"></Button>
-          <Button baseColor="teal" image="Download.svg" text="Save"></Button>
+          <Button baseColor="teal" image="Download.svg" text="Save" onClick={createEditNote}></Button>
         </div>
       </div>
     </div>
