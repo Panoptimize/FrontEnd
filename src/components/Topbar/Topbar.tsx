982e24049dee5872251613885ec5ab88c45749bf
Klaus Cedillo <klausmcedillo@gmail.com>
2024-06-03 20:22:08 -0600

    Organization
    Organize the variables by name

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -21,7 +21,9 @@ const Topbar: React.FC<TopbarProps> = ({ toggleSidebar, variant = 0 }) => {
     displayOption = "block";
   }
 
-  const [userInfo, setUserInfo] = useState<any[]>([]);
+  const [name, setName] = useState<any>();
+  const [email, setEmail] = useState<string>();
+  const [imagePath, setImagePath] = useState<string>();
 
   const getUserInfo = async() =>{
     await getSupervisorInfo()
@@ -29,7 +31,9 @@ const Topbar: React.FC<TopbarProps> = ({ toggleSidebar, variant = 0 }) => {
       if(data && data.data){
         console.log("data.data.results");
         console.log(data);
-        setUserInfo(data.data.results);
+        setName(data.data.fullname);
+        setEmail(data.data.email);
+        setImagePath(data.data.imagePath);
       }
     })
     .catch((err) => {
@@ -60,7 +64,7 @@ const Topbar: React.FC<TopbarProps> = ({ toggleSidebar, variant = 0 }) => {
         <div className="flex flex-row space-x-2">
           <p className="text-gray-600 font-medium text-lg ml-1">Welcome</p>
           {/* ======================== NAME ======================== */}
-          <p className="font-medium text-lg">John</p>
+          <p className="font-medium text-lg">{name}</p>
         </div>
       </div>
       <div className="h-16 flex items-center space-x-5">
@@ -76,16 +80,16 @@ const Topbar: React.FC<TopbarProps> = ({ toggleSidebar, variant = 0 }) => {
         </div>
         <div className="h-full flex flex-col justify-center items-center pr-7">
           <div className="h-full flex flex-col justify-center">
-          {/* ======================== NAME ======================== */}
-            <p className="text-left text-lg">John Connor</p>
+          {/* ======================== FULL NAME ======================== */}
+            <p className="text-left text-lg">{name}</p>
           {/* ======================== EMAIL ======================== */}
             <p className="text-gray-600 text-left text-xs">
-              jconnor28@gmail.com
+              {email}
             </p>
           </div>
         </div>
         <div className="items-center flex pr-5">
-          <Avatar />
+          <Avatar profile_img={imagePath}/>
         </div>
       </div>
     </div>
