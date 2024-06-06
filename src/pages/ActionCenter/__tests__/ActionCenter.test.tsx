b85568667be1cee102ff9f5c746aee5314c5f8b5
Raul V <74110279+Raul-VR@users.noreply.github.com>
2024-06-06 12:27:10 -0600

    Functioning test

--- src/pages/ActionCenter/__tests__/ActionCenter.test.tsx
+++ src/pages/ActionCenter/__tests__/ActionCenter.test.tsx
@@ -5,7 +5,7 @@ import ActionCenter from "../ActionCenter";
 import mockAxios from 'jest-mock-axios';
 import { getStatus } from "../../../services/status/getStatus";
 import {mockStatusCard} from "../../../services/status/_mocks_/statusResults"
-jest.mock("../../../services");
+//jest.mock("../../../services");
 
 // jest.mock('../../../services', () => ({
 //     getStatus: jest.fn()
@@ -16,10 +16,10 @@ jest.mock("../../../services");
 //   getStatus: jest.fn().mockResolvedValue({ error: false }),
 // }));
 
-jest.mock('../../../services', () => ({
-  getStatus: jest.fn(),
-}));
-  
+// jest.mock('../../../services', () => ({
+//   getStatus: jest.fn(),
+// }));
+
 
 // beforeEach(() => {
 //     (getStatus as jest.Mock).mockResolvedValue({
@@ -42,48 +42,5 @@ describe("ActionCenter", () => {
   });
 
     
-  test("The Status Card renders correctly", async () => {
-
-    // const mockResponse = {
-    //     data: [
-    //         { status: 'AGENTS', numUsers: 34 },
-    //         { status: 'AGENTS_ONLINE', numUsers: 2 },
-    //         { status: 'AGENTS_AVAILABLE', numUsers: 2 },
-    //         { status: 'AGENTS_OFFLINE', numUsers: 32 },
-    //     ],
-    //     status: 200,
-    //     statusText: 'OK',
-    //     headers: {},
-    //     config: {},
-    // };
-
-    // mockAxios.get.mockResolvedValueOnce(mockResponse);
-
-    (getStatus as jest.Mock).mockResolvedValue({
-      data: mockStatusCard ,
-      error: null,
-    });
-
-
-    render(<ActionCenter />);
-
-    await screen.findByTestId('wrapper-ActionCenter');
-
-    //const statusCardHolder = screen.getByTestId('mock-status-card-holder');
-    //expect(statusCardHolder).toBeInTheDocument();
-    expect(screen.getByText('Agents')).toBeInTheDocument();
-    expect(screen.getByText('Online')).toBeInTheDocument();
-
-
-    //expect(screen.getByTestId("txt-agentStatus")).toHaveTextContent("Agents Status");
-
-    // expect(screen.getByTestId("txt-agentStatus")).toHaveTextContent(" Agents Status ");
-    // expect(screen.getByTestId("txt-statudCardHolder")).toBeInTheDocument();
-
-    // await waitFor(() => {
-    //     expect(screen.getByText(expectedText)).toBeInTheDocument();
-    //   });
-
-  });
 
 });
