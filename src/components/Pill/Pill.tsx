b015a9ab292fcfb60e3dd8cb3d496449ad68ec39
BillySh <A01657143@tec.mx>
2024-06-06 19:58:34 -0600

    PillTest

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -6,7 +6,7 @@ import { Button } from "../Button";
 const Pill: React.FC<IPill> = ({ title, icon }) => {
   const isIcon = icon?.endsWith(".svg");
   if (icon && !isIcon) {
-    console.error("simple pill");
+    // console.error("simple pill");
     return null;
   }
 
@@ -20,7 +20,8 @@ const Pill: React.FC<IPill> = ({ title, icon }) => {
               className="img"
               src={require(`../../assets/images/${icon}`)}
               alt={icon}
-            ></img>
+              datatest-id="img-pill"
+            />
           </button>
         )}
       </div>
