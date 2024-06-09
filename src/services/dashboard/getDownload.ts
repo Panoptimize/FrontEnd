1308425ad58e214afd0b80767353a77fefe9b456
PabloCeb2000 <113377312+PabloCeb2000@users.noreply.github.com>
2024-06-09 04:56:31 -0600

    Finally, the download now woks completly with the dashboard

--- src/services/dashboard/getDownload.ts
+++ src/services/dashboard/getDownload.ts
@@ -1,17 +1,20 @@
 import httpInstance from "../httpInstance";
 
 interface DownloadDTO {
+    instanceId: string;
     startDate: string;
     endDate: string;
     routingProfiles: string[];
     queues: string[];
-  }
+}
 
 export const getDownload = async (sDate: string, eDate: string, workspace: string[]) => {
-    const endpoint = '/download/getDownload/7c78bd60-4a9f-40e5-b461-b7a0dfaad848';
+    const endpoint = '/download/getDownload';
     const date = new Date();
+    const newDate = new Date(date.getTime());
 
     const downloadDTO: DownloadDTO = {
+        instanceId: '7c78bd60-4a9f-40e5-b461-b7a0dfaad848',
         startDate: sDate,
         endDate: eDate,
         routingProfiles: workspace,
@@ -28,7 +31,7 @@ export const getDownload = async (sDate: string, eDate: string, workspace: strin
             
             const link = document.createElement('a');
             link.href = url;
-            link.download = 'DataReport_' + date.getTime() + '.xlsx'; 
+            link.download = 'DataReport_' + newDate + '.xlsx'; 
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
