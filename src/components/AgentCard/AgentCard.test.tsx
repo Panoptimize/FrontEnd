8e49f1f047a2473d166e0134f3e8190cbd27dd77
A01661651 <105233241+A01661651@users.noreply.github.com>
2024-06-10 02:21:32 -0600

    Aumento en % Test

--- src/components/AgentCard/AgentCard.test.tsx
+++ src/components/AgentCard/AgentCard.test.tsx
@@ -4,12 +4,11 @@ import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import AgentCard from './AgentCard';
 import { getAgentNotes } from '../../services/notes/getAgentNotes';
 import { getAgentId } from '../../services/agentsList/getAgentId';
-
-
+import profilePicture from "../../assets/images/Toretto.jpg";
 
 // Mockear las funciones y servicios necesarios
 jest.mock('../../services/notes/getAgentNotes', () => ({
-  getAgentNotes: jest.fn().mockResolvedValue({ data: { content: [] } }),
+  getAgentNotes: jest.fn().mockResolvedValue({ data: { content: ['Note 1 Content', 'Note 2 Content'] } }),
 }));
 
 jest.mock('../../services/agentsList/getAgentId', () => ({
@@ -19,8 +18,6 @@ jest.mock('../../services/agentsList/getAgentId', () => ({
 describe('AgentCard Component', () => {
   it('renders with default props', () => {
     const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-    // Agrega la prop 'id' con un valor arbitrario, por ejemplo "123"
-
     expect(getByText('Test Name')).toBeInTheDocument();
     expect(getByText('Test Workspace')).toBeInTheDocument();
   });
@@ -28,191 +25,156 @@ describe('AgentCard Component', () => {
   it('opens modal on button click', async () => {
     const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
     const button = getByText('View Details');
-  
     fireEvent.click(button);
-  
     const modalTitle = await findByText('Contact Details');
     expect(modalTitle).toBeInTheDocument();
   });
 
   it('closes modal on close button click', async () => {
-    const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const { getByText, findByRole } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
     const button = getByText('View Details');
-  
     fireEvent.click(button);
-  
-    const closeButton = await findByText('Close Button Text'); // Asegúrate de ajustar el texto del botón de cierre según tu implementación
+    const closeButton = await findByRole('button', { name: /cross/i });
     fireEvent.click(closeButton);
-  
-    expect(closeButton).not.toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText('Contact Details')).not.toBeInTheDocument();
+    });
   });
 
   it('loads agent data correctly', async () => {
     const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
     const button = getByText('View Details');
-  
     fireEvent.click(button);
-  
     const agentName = await findByText('Test Name');
     const agentWorkspace = await findByText('Test Workspace');
-  
     expect(agentName).toBeInTheDocument();
     expect(agentWorkspace).toBeInTheDocument();
+  });
+
+  it('calls getId function when opening modal', async () => {
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(getAgentId).toHaveBeenCalledWith("123");
+    });
+  });
+
+  it('handles error in getId function', async () => {
+    (getAgentId as jest.Mock).mockRejectedValueOnce(new Error('Error fetching agent id'));
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(consoleErrorSpy).toHaveBeenCalledWith(new Error('Error fetching agent id'));
+    });
+    consoleErrorSpy.mockRestore();
   });
 
   it('loads agent notes correctly', async () => {
     const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
     const button = getByText('View Details');
-  
     fireEvent.click(button);
-  
-    // Simula la obtención de notas del agente
-  
     const note1 = await findByText('Note 1 Content');
     const note2 = await findByText('Note 2 Content');
-  
     expect(note1).toBeInTheDocument();
     expect(note2).toBeInTheDocument();
   });
-  
+
+  it('calls getNotes function when getId is successful', async () => {
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(getAgentNotes).toHaveBeenCalledWith(123);
+    });
+  });
+
+  it('handles error in getNotes function', async () => {
+    (getAgentNotes as jest.Mock).mockRejectedValueOnce(new Error('Error fetching notes'));
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(consoleErrorSpy).toHaveBeenCalledWith(new Error('Error fetching notes'));
+    });
+    consoleErrorSpy.mockRestore();
+  });
+
   it('renders notes only when modal is open', async () => {
     const { getByText, queryByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
     const button = getByText('View Details');
-  
-    // Verifica que las notas no estén presentes antes de abrir el modal
     expect(queryByText('Note 1 Content')).not.toBeInTheDocument();
-  
     fireEvent.click(button);
-  
-    // Verifica que las notas estén presentes después de abrir el modal
-    expect(getByText('Note 1 Content')).toBeInTheDocument();
+    expect(await screen.findByText('Note 1 Content')).toBeInTheDocument();
   });
 
-    it('renders notes only when modal is open', async () => {
-        const { getByText, queryByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-    
-        // Verifica que las notas no estén presentes antes de abrir el modal
-        expect(queryByText('Note 1 Content')).not.toBeInTheDocument();
-    
-        fireEvent.click(button);
-    
-        // Verifica que las notas estén presentes después de abrir el modal
-        expect(getByText('Note 1 Content')).toBeInTheDocument();
+  it('displays loading indicator while agent data is being fetched', async () => {
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    expect(getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('displays error message when agent data fails to load', async () => {
+    (getAgentId as jest.Mock).mockRejectedValueOnce(new Error('Error fetching agent data'));
+    const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+    const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    await waitFor(() => {
+      expect(getByText('Error: Unable to load agent data')).toBeInTheDocument();
+      expect(consoleErrorSpy).toHaveBeenCalledWith(new Error('Error fetching agent data'));
     });
+    consoleErrorSpy.mockRestore();
+  });
 
-    it('requests agent notes when opening modal', async () => {
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-      
-        fireEvent.click(button);
-      
-        // Simula la obtención de notas del agente y verifica que se llama a la función correspondiente
-        expect(getAgentNotes).toHaveBeenCalledWith(123);
-      }); 
-          
+  it('closes modal when "Esc" key is pressed', async () => {
+    const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
+    const button = getByText('View Details');
+    fireEvent.click(button);
+    fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' });
+    await waitFor(() => {
+      expect(screen.queryByText('Contact Details')).not.toBeInTheDocument();
+    });
+  });
 
-      
+  // Storybook
+  it('renders CreateNewAgent story correctly', () => {
+    const { getByText, getByAltText } = render(<AgentCard
+      id="create-new-agent-id"
+      title="Create New Agent"
+      name="Name"
+      email="Email"
+      username="Username"
+      profileImage={profilePicture}
+      workspace="Sales"
+    />);
+    expect(getByText('Create New Agent')).toBeInTheDocument();
+    expect(getByText('Name')).toBeInTheDocument();
+    expect(getByText('Email')).toBeInTheDocument();
+    expect(getByText('Username')).toBeInTheDocument();
+    expect(getByAltText('profile')).toHaveAttribute('src', profilePicture);
+    expect(getByText('Sales')).toBeInTheDocument();
+  });
 
-
-
-
-      it('fetches agent notes correctly', async () => {
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-      
-        fireEvent.click(button);
-      
-        // Esperar a que se resuelva la promesa de obtención del ID del agente
-        await Promise.resolve();
-      
-        // Verificar que se llamó a la función de obtención de notas del agente con el ID correcto
-        expect(getAgentNotes).toHaveBeenCalledWith(123);
-      });
-
-      it('fetches agent notes correctly', async () => {
-        // Configurar el mock para que resuelva la promesa con datos simulados
-        (getAgentNotes as jest.Mock).mockResolvedValueOnce({ data: { content: ['Note 1', 'Note 2'] } });
-    
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-    
-        fireEvent.click(button);
-    
-        // Esperar a que se resuelva la promesa
-        await waitFor(() => {
-          // Verificar que se llamó a la función de obtención de notas del agente con el ID correcto
-          expect(getAgentNotes).toHaveBeenCalledWith(123);
-          // Verificar que se establecieron correctamente los datos de las notas en el estado
-          expect(getByText('Note 1')).toBeInTheDocument();
-          expect(getByText('Note 2')).toBeInTheDocument();
-        });
-      });
-    
-      it('handles error when fetching agent notes', async () => {
-        // Configurar el mock para que rechace la promesa con un error simulado
-        (getAgentNotes as jest.Mock).mockRejectedValueOnce(new Error('Error fetching notes'));
-    
-        const consoleErrorSpy = jest.spyOn(console, 'error');
-        consoleErrorSpy.mockImplementation(() => {}); // Evitar que se imprima el error en la consola
-    
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-    
-        fireEvent.click(button);
-    
-        // Esperar a que se maneje el error y se llame a console.error
-        await waitFor(() => {
-          expect(consoleErrorSpy).toHaveBeenCalledWith(new Error('Error fetching notes'));
-        });
-      });
-
-
-
-
-      it('displays loading indicator while agent data is being fetched', async () => {
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-      
-        fireEvent.click(button);
-      
-        // Verifica que el indicador de carga esté presente mientras se cargan los datos del agente
-        expect(getByText('Loading...')).toBeInTheDocument();
-      });
-      
-      it('displays error message when agent data fails to load', async () => {
-        // Configura el mock para que falle la obtención de datos del agente
-        (getAgentId as jest.Mock).mockRejectedValueOnce(new Error('Error fetching agent data'));
-      
-        const consoleErrorSpy = jest.spyOn(console, 'error');
-        consoleErrorSpy.mockImplementation(() => {}); // Evitar que se imprima el error en la consola
-      
-        const { getByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-      
-        fireEvent.click(button);
-      
-        // Espera a que se maneje el error y muestra el mensaje de error
-        await waitFor(() => {
-          expect(getByText('Error: Unable to load agent data')).toBeInTheDocument();
-          expect(consoleErrorSpy).toHaveBeenCalledWith(new Error('Error fetching agent data'));
-        });
-      });
-      
-      it('closes modal when "Esc" key is pressed', async () => {
-        const { getByText, findByText } = render(<AgentCard id="123" name="Test Name" workspace="Test Workspace" />);
-        const button = getByText('View Details');
-      
-        fireEvent.click(button);
-      
-        // Simula la pulsación de la tecla "Esc"
-        fireEvent.keyDown(document, { key: 'Escape', code: 'Escape' });
-      
-        // Verifica que el modal se haya cerrado
-        await waitFor(() => {
-          expect(screen.queryByText('Contact Details')).not.toBeInTheDocument();
-        });
-      });
-      
-      
+  it('renders EditAgentDetails story correctly', () => {
+    const { getByText, getByAltText } = render(<AgentCard
+      id="edit-agent-details-id"
+      title="Edit Agent Details"
+      name="John Doe"
+      email="johndoe@example.com"
+      username="johndoe"
+      profileImage={profilePicture}
+      workspace="Sales"
+    />);
+    expect(getByText('Edit Agent Details')).toBeInTheDocument();
+    expect(getByText('John Doe')).toBeInTheDocument();
+    expect(getByText('johndoe@example.com')).toBeInTheDocument();
+    expect(getByText('johndoe')).toBeInTheDocument();
+    expect(getByAltText('profile')).toHaveAttribute('src', profilePicture);
+    expect(getByText('Sales')).toBeInTheDocument();
+  });
 });
