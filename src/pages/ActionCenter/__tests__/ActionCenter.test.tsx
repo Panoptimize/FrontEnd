9e72083c06c3b56f13d43ae074be3ac96ffdb29c
Ernesto Miranda <ernomitrovic@zohomail.com>
2024-06-10 21:13:33 -0600

    Coverage added
    Co-Authored-By: Raul V <74110279+Raul-VR@users.noreply.github.com>

--- src/pages/ActionCenter/__tests__/ActionCenter.test.tsx
+++ src/pages/ActionCenter/__tests__/ActionCenter.test.tsx
@@ -1,76 +1,67 @@
-import { act, cleanup, render, screen, waitFor } from "@testing-library/react";
-
+import { act, cleanup, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import ActionCenter from "../ActionCenter";
-import { getStatus } from "../../../services";
-import {mockStatusCard} from "../../../services/status/_mocks_/statusResults"
-jest.mock("../../../services");
+import { mockActionCenterData, mockAgentsData, mockStatusCard, mockStatusData } from "../../../services/status/_mocks_/statusResults";
+import { getActionCenter } from "../../../services/actionCenter/getActionCenter";
+import { getStatus } from "../../../services/status/getStatus";
+import { getAgentsList } from "../../../services/agentsList/getAgentsList";
 
-// jest.mock('../../../services', () => ({
-//     getStatus: jest.fn()
-// }));
-
-
-// jest.mock('../../../services', () => ({
-//   getStatus: jest.fn().mockResolvedValue({ error: false }),
-// }));
-
-/* jest.mock('../../../services/status/getStatus.ts', () => ({
-  (getStatus as jest.Mock).mockResolvedValue({
-    data: mockStatusCard,
-    error: null,
+jest.mock("../../../services/actionCenter/getActionCenter");
+jest.mock("../../../services/status/getStatus");
+jest.mock("../../../services/agentsList/getAgentsList");
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useOutletContext: () => ({
+    setNotifications: jest.fn()
   }),
-})); */
-
-
-// beforeEach(() => {
-//     (getStatus as jest.Mock).mockResolvedValue({
-//         data: { results: mockStatusCard },
-//       });
-// });
+}));
 
 beforeEach(() => {
   cleanup();
+  sessionStorage.clear();
 });
 
 describe("ActionCenter", () => {
+  beforeEach(() => {
+    (getStatus as jest.Mock).mockResolvedValue({ data: mockStatusData, error: null });
+    (getAgentsList as jest.Mock).mockResolvedValue(mockAgentsData);
+    (getActionCenter as jest.Mock).mockResolvedValue(mockActionCenterData);
+  });
 
   test("The ActionCenter renders correctly", async () => {
     render(<ActionCenter />);
     expect(screen.getByTestId("wrapper-ActionCenter")).toBeTruthy();
-
   });
-
 
   test("The Status Card renders correctly", async () => {
-    (getStatus as jest.Mock).mockResolvedValue(
-      {
-        data: mockStatusCard, error: null 
-      });
-
     render(<ActionCenter />);
 
-
-    //const statusCardHolder = screen.getByTestId('mock-status-card-holder');
-    //expect(statusCardHolder).toBeInTheDocument();
-
     await waitFor(() => {
-      screen.findByTestId('wrapper-ActionCenter');
-
+      expect(screen.getByTestId('wrapper-ActionCenter')).toBeInTheDocument();
+      // Check if Status Cards are rendered correctly
+      mockStatusCard.forEach(item => {
+        expect(screen.getByText(item.status)).toBeInTheDocument();
+      });
     });
-
-
-
-
-    //expect(screen.getByTestId("txt-agentStatus")).toHaveTextContent("Agents Status");
-
-    // expect(screen.getByTestId("txt-agentStatus")).toHaveTextContent(" Agents Status ");
-    // expect(screen.getByTestId("txt-statudCardHolder")).toBeInTheDocument();
-
-    // await waitFor(() => {
-    //     expect(screen.getByText(expectedText)).toBeInTheDocument();
-    //   });
-
   });
 
+  test('Handles reset button click', async () => {
+    render(<ActionCenter />);
+
+    await waitFor(() => {
+      expect(getStatus).toHaveBeenCalled();
+      expect(getAgentsList).toHaveBeenCalled();
+      expect(getActionCenter).toHaveBeenCalled();
+    });
+
+    act(() => {
+      const resetButton = screen.getByTestId('reset-button');
+      fireEvent.click(resetButton);
+      expect(sessionStorage.getItem('rows')).toBeNull();
+    });
+
+    await waitFor(() => {
+      expect(getActionCenter).toHaveBeenCalled();
+    });
+  });
 });
